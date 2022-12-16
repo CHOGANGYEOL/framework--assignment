f20705446def14cf2f325980a40466c4e49e8402
CHOGANGYEOL <loeygnak@gmail.com>
2022-12-16 14:20:12 +0900

    reduxComplete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrease, increase } from './Actions';
+import styled from 'styled-components';
+
+const BtnWrap = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 130px;
+  justify-content: space-between;
+`;
+
+const Btn = styled.button`
+  width: 50px;
+  height: 50px;
+  border: 0;
+  border-radius: 50%;
+  color: #292c33;
+  font-size: 24px;
+  background-color: #82d7f7;
+  cursor: pointer;
+`;
 
 function App() {
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const plusNum = () => {
+    dispatch(increase());
+  };
+
+  const minusNum = () => {
+    dispatch(decrease());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{`Count: ${state}`}</p>
+        <BtnWrap>
+          <Btn onClick={plusNum}>+</Btn>
+          <Btn onClick={minusNum}>-</Btn>
+        </BtnWrap>
       </header>
     </div>
   );
